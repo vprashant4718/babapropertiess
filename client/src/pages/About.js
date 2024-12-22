5990b2bc810a50d53f2d758ea7a1b2e0d51a83cb
Prashant Verma <127789716+vprashant4718@users.noreply.github.com>
2024-12-22 21:13:15 +0530

    Update About.js
    change about section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,12 +8,9 @@ export default function About() {
           Verma Builders
         </h1>
         <br />
-        <p className="text-slate-600 font-semibold text-sm lg:w-[70vw]" > 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, et.
-          Velit omnis distinctio iste soluta similique ducimus facere dolor
-          odit, id aspernatur, perspiciatis repudiandae doloribus voluptates
-          reprehenderit eos qui nam. d.
-        </p>
+        <h4 className="text-slate-600 font-semibold text-sm lg:w-[70vw]" > 
+         verma builders provide a dynamic, scalable, and feature-rich platform for managing property listings, interacting with potential buyers or renters, and facilitating the process of buying, selling, or renting real estate. Build a seamless experience with real-time updates, user-friendly interfaces, and efficient back-end processing, all while using a modern technology stack that simplifies development and scalability.
+        </h4>
         </div>
         </div>
   )
